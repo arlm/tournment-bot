306b576a02782a0ce007f57a6f7c4cbfedeb19ba
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@keyidentity.com>
2018-05-10 21:17:57 +0200

    First commit

---
+++ Telegram.Tournment.Bot/Bot.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace Bot.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ Telegram.Tournment.Bot/Telegram.Bot/WebHook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+
+[assembly: LambdaSerializer(typeof(JsonSerializer))]
+
+namespace Telegram.Bot
+{
+	public class WebHook
+	{
+		public APIGatewayProxyResponse Post(APIGatewayProxyRequest request, ILambdaContext context)
+		{
+			// Log entries show up in CloudWatch
+			context.Logger.LogLine("Example log entry\n");
+
+			var response = new APIGatewayProxyResponse
+			{
+				StatusCode = (int)HttpStatusCode.OK,
+				Body = "{ \"Message\": \"Hello World\" }",
+				Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+			};
+
+			return response;
+		}
+	}
+}
